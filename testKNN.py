d8bb312cdd86f7c1ea2a40dea213f479b6be9353
Basia <barbara.zakrz@gmail.com>
2023-04-05 12:19:01 +0200

    normalizacja

--- testKNN.py
+++ testKNN.py
@@ -17,14 +17,32 @@ class Probki:
             reader = f.readlines()
             for line in reader:
                 line = line.rstrip('\n')
-                wiersz = []
-                wiersz = line.split('\t')
-                items.append(wiersz)
+                items.append(line.split('\t'))
 
         for item in items:
             Probki(
                 grupa = item.pop(len(item)-1),
-                par = [float(x) for x in item],)
+                par = [float(x) for x in item])
+
+    @classmethod
+    def normalizacja(cls):
+        min = Probki.lista_probek[0].par.copy()
+        max  =Probki.lista_probek[0].par.copy()
+
+        for i in Probki.lista_probek:
+            for j in range(len(i.par)):
+                if i.par[j] < min [j]:
+                    min[j] = i.par[j]
+                if i.par[j] > max [j]:
+                    max[j] = i.par[j]
+        #print(min, max)
+        for i in Probki.lista_probek:
+            for j in range(len(i.par)):
+                i.par[j] = (i.par[j] - min[j])/(max[j]-min[j])
+        return Probki.lista_probek
+
+
+
 
     def __repr__(self):
         return 'Probka({}, grupa: {}'.format(self.par, self.grupa)
@@ -32,6 +50,10 @@ class Probki:
 
 Probki.wczytaj_z_pliku()
 print(Probki.lista_probek)
+Probki.normalizacja()
+print(Probki.lista_probek)
+
+
 
 
 
